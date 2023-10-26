2a824d6d814b03c66ca05cbeaf8e804861abd276
Nano <luxorius07@gmail.com>
2023-10-26 13:37:57 -0300

    se realizo el ejercicio 2 con su consigna

---
+++ js/ejercicio2.js
@@ -0,0 +1,12 @@
+/*
+Ejercicio 2: Concatenación de Cadenas
+
+Crea dos variables, nombre y apellido, y asígnales valores de tu elección. Luego, crea una tercera variable llamada nombreCompleto que contenga la concatenación de nombre y apellido. Muestra nombreCompleto en la consola.
+*/
+
+const nombre = 'Humberto';
+
+const Apellido = 'Moreno';
+
+const nombreCompleto = nombre +' '+Apellido;
+console.log(nombreCompleto);
