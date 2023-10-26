028b16e44d363e456f47b61d9d9ba1541206ca5b
Nano <luxorius07@gmail.com>
2023-10-26 13:27:47 -0300

    se realizaron los puntos del ejercicio1

---
+++ js/ejercicio1.js
@@ -0,0 +1,32 @@
+/*
+Ejercicio 1: Variables y Operaciones Matemáticas
+
+Crea dos variables, numero1 y numero2, y asígnales valores numéricos. Luego, realiza las siguientes operaciones y muestra los resultados en la consola:
+* Suma de numero1 y numero2.
+* Resta de numero1 y numero2.
+* Multiplicación de numero1 y numero2.
+* División de numero1 entre numero2.
+*/
+
+const numero1 = 5
+
+const numero2 = 2
+
+//Suma de numero1 y numero2.
+
+resultado = numero1 + numero2;
+console.log('Resultado: '+ resultado);
+
+//Resta de numero1 y numero2.
+
+resultado = numero1 - numero2;
+console.log('Resultado: '+ resultado);
+
+//Multiplicación de numero1 y numero2.
+resultado = numero1 * numero2;
+console.log('Resultado: '+ resultado);
+
+//División de numero1 entre numero2.
+
+resultado = numero1 / numero2;
+console.log('Resultado: '+ resultado);
