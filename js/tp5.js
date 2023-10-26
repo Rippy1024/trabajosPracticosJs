e70ae1b8e89ed52b41c4986712d122e38b1132c1
Nano <luxorius07@gmail.com>
2023-10-26 16:35:30 -0300

    se realizo el ejercicio 5 del practico

---
+++ js/tp5.js
@@ -0,0 +1,8 @@
+/*
+5.- Escribe un programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números.
+Nota: Tener en cuenta la siguiente función: parseInt
+https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Objetos_globales/parseInt
+*/
+let numero1 = parseInt(prompt("Por favor, introduce el primer número:"));
+let numero2 = parseInt(prompt("Por favor, introduce el segundo número:"));
+document.write("La suma de los números es: " + (numero1 + numero2));
